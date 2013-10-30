5c9c2def7e4d19b04c5e606c0f8042fcd8c59d50
Kevin James Matzen <kmatzen@cs.cornell.edu>
2013-10-30 15:45:17 -0400

    Cleaned up geo_rescore.

--- geo_rescore.py
+++ geo_rescore.py
@@ -3,14 +3,9 @@
 import scores
 
 # standard imports
-import sys
-import tempfile
 import logging
 import argparse
-import os
 import math
-import ConfigParser
-from collections import namedtuple
 
 # nyc3dcars imports
 import nyc3dcars
@@ -18,59 +13,55 @@ import nyc3dcars
 # math imports
 import numpy
 
-import os
-
 from celery.task import task
 
-from sqlalchemy import *
-from sqlalchemy.orm import *
+from sqlalchemy import func, or_
+#from sqlalchemy.orm import *
+
 
 @task
 def geo_rescore(pid, model, method):
-    logging.info('geo_rescore %d %s %s'%(pid, model, method))
+    logging.info('geo_rescore %d %s %s' % (pid, model, method))
     session = nyc3dcars.Session()
     try:
-        photo = session.query(nyc3dcars.Photo) \
-            .filter_by(id=pid) \
-            .one()
-
         numpy.seterr(all='raise')
 
         session.query(nyc3dcars.Model) \
             .filter(nyc3dcars.Model.filename == model) \
             .one()
 
-        nms_method = scores.Methods[method]
+        nms_method = scores.__Methods__[method]
 
         detections = session.query(nyc3dcars.Detection) \
             .join(nyc3dcars.Model) \
             .filter(nyc3dcars.Detection.pid == pid) \
             .filter(nyc3dcars.Model.filename == model) \
-            .filter(or_(*[m == None for m in nms_method.inputs])) 
+            .filter(or_(*[m == None for m in nms_method.inputs]))
 
-        nms_method = scores.Methods[method]
+        nms_method = scores.__Methods__[method]
 
         for method_input in nms_method.inputs:
             score_name = str(method_input).split('.')[-1]
-            score = scores.Scores[score_name]
+            score = scores.__Scores__[score_name]
 
             if score.compute is None:
                 continue
 
-            for pos, detection in enumerate(detections):
-                    value = score.compute(session, detection)
+            for detection in detections:
+                value = score.compute(session, detection)
 
-                    existing = getattr(detection, score_name)
+                existing = getattr(detection, score_name)
 
-                    if existing is not None:
-                        if not math.fabs(existing - value) < 1e-8:
-                            logging.info('%s %f %f'%(score_name, existing, value))
-                            assert False
+                if existing is not None:
+                    if not math.fabs(existing - value) < 1e-8:
+                        logging.info(
+                            '%s %f %f' % (score_name, existing, value))
+                        assert False
 
-                    setattr(detection, score_name, value)
+                setattr(detection, score_name, value)
 
         session.commit()
-    except Exception, exc:
+    except Exception:
         session.rollback()
         raise
     finally:
@@ -83,7 +74,8 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('--pid', type=int, required=True)
     parser.add_argument('--model', required=True)
-    parser.add_argument('--method', required=True, choices=scores.Methods.keys())
+    parser.add_argument(
+        '--method', required=True, choices=scores.__Methods__.keys())
     args = parser.parse_args()
 
     geo_rescore(**vars(args))
