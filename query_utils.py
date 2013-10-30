47f08444892c53b9da6938b4470b48670976a32b
Kevin James Matzen <kmatzen@cs.cornell.edu>
2013-10-30 16:14:11 -0400

    Cleaned up query utils.

--- query_utils.py
+++ query_utils.py
@@ -1,41 +1,45 @@
-from sqlalchemy import *
+from sqlalchemy import func
 
-def overlap(geomA, geomB):
-    intersection_score = intersection(geomA, geomB)
 
-    area1 = (geomA.x2 - geomA.x1) * (geomA.y2 - geomA.y1)
+def overlap(geom_a, geom_b):
+    intersection_score = intersection(geom_a, geom_b)
 
-    area2 = (geomB.y2 - geomB.y1) * (geomB.x2 - geomB.x1)
+    area1 = (geom_a.x2 - geom_a.x1) * (geom_a.y2 - geom_a.y1)
+
+    area2 = (geom_b.y2 - geom_b.y1) * (geom_b.x2 - geom_b.x1)
 
     union_score = area1 + area2 - intersection_score
 
-    overlap_score = intersection_score/union_score
+    overlap_score = intersection_score / union_score
 
     return overlap_score
 
-def overlap_asym(geomA, geomB):
-    intersection_score = intersection(geomA, geomB)
 
-    area2 = (geomB.y2 - geomB.y1) * (geomB.x2 - geomB.x1)
+def overlap_asym(geom_a, geom_b):
+    intersection_score = intersection(geom_a, geom_b)
 
-    overlap_score = intersection_score/area2
+    area2 = (geom_b.y2 - geom_b.y1) * (geom_b.x2 - geom_b.x1)
+
+    overlap_score = intersection_score / area2
 
     return overlap_score
 
-def intersection(geomA, geomB):
-    intersection_score = func.greatest(0, (func.least(geomA.x2, geomB.x2) - 
-                                     func.greatest(geomA.x1, geomB.x1))) * \
-                   func.greatest(0, (func.least(geomA.y2, geomB.y2) -
-                                     func.greatest(geomA.y1, geomB.y1)))
+
+def intersection(geom_a, geom_b):
+    intersection_score = func.greatest(0, (func.least(geom_a.x2, geom_b.x2) -
+                                           func.greatest(geom_a.x1, geom_b.x1))) * \
+        func.greatest(0, (func.least(geom_a.y2, geom_b.y2) -
+                          func.greatest(geom_a.y1, geom_b.y1)))
 
     return intersection_score
 
-def union(geomA, geomB):
-    intersection_score = intersection(geomA, geomB)
 
-    area1 = (geomA.x2 - geomA.x1) * (geomA.y2 - geomA.y1)
+def union(geom_a, geom_b):
+    intersection_score = intersection(geom_a, geom_b)
 
-    area2 = (geomB.y2 - geomB.y1) * (geomB.x2 - geomB.x1)
+    area1 = (geom_a.x2 - geom_a.x1) * (geom_a.y2 - geom_a.y1)
+
+    area2 = (geom_b.y2 - geom_b.y1) * (geom_b.x2 - geom_b.x1)
 
     union_score = area1 + area2 - intersection_score
 
