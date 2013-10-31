bf0199558fff4e9926a6857e5fd5e74a5cce1717
Kevin James Matzen <kmatzen@cs.cornell.edu>
2013-10-31 11:56:42 -0400

    Removed comment.

--- nyc3dcars.py
+++ nyc3dcars.py
@@ -107,24 +107,3 @@ class ElevationRaster(BASE):
 BASE.metadata.create_all(ENGINE)
 SESSION = scoped_session(sessionmaker(bind=ENGINE))
 
-"""
-from sqlalchemy import event
-from sqlalchemy.engine import Engine
-import time
-import logging
-logger = logging.getLogger("myapp.sqltime")
-logger.setLevel(logging.DEBUG)
-
-@event.listens_for(Engine, "before_cursor_execute")
-def before_cursor_execute(conn, cursor, statement, 
-                        parameters, context, executemany):
-    context._query_start_time = time.time()
-    logger.debug("Start Query: %s" % statement)
-
-@event.listens_for(Engine, "after_cursor_execute")
-def after_cursor_execute(conn, cursor, statement, 
-                        parameters, context, executemany):
-    total = time.time() - context._query_start_time
-    logger.debug("Query Complete!")
-    logger.debug("Total Time: %f" % total)
-"""
